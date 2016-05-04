0161c2dab2cdbd1c521c15e905a59ea432289554
mpoc <ozkoskiautas@gmail.com>
2016-05-05 00:59:08 +0300

    Program actually works

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 /*
 TODO:
-make flight class 3, for function printStatusOfSeats() to print seats in both classes
+make flight class 3 (0?), for function printStatusOfSeats() to print seats in both classes
 */
 
 #include <iostream>
@@ -9,25 +9,43 @@ make flight class 3, for function printStatusOfSeats() to print seats in both cl
 
 using namespace std;
 
-int flightClass;                            // First/economy
+int flightClass;                            // First/second
 int seatPick;                               // Which seat you pick (1-10). Also used to exit loops (when value is 0, certain loops are broken)
 int switchClass;                            // Variable used to switch classes
 //int reserveAnother=1;                     // Integer which changes according to client's need to reserve another seat (1=reserve one more, 0=stop reserving)
-const int firstClassSeats[2]={1,5};         // Settings for the seats of the First class (first number - "from" seat, second - "to" seat)
-const int economyClassSeats[2]={6,10};      // Settings for the seats of the Economy class (first number - "from" seat, second - "to" seat)
-    /*  It is assumed that:
-            First class seat indexes will be smaller than the indexes of the Economy class
-            Both for First and Economy class, the first ("from") number will be smaller than the second ("to")
-    */
-bool seatStatus[10];                        // Taken/free. Set seatStatus[] to whatever value economyClassSeats[1] is
+const int firstClassSeats[2]={1,5};         // Settings for the seats of the First class (first number - "from" seat, second - "to" seat). These numbers are INCLUSIVE
+const int secondClassSeats[2]={6,10};       // Settings for the seats of the Economy class (first number - "from" seat, second - "to" seat). These numbers are INCLUSIVE
+/*  It is assumed that:
+        First class "to" seat will be 1 smaller than the "from" seat of the second class (ignoring this will cause empty indexes)
+        Both for First and Economy class, the first ("from") number will be smaller than the second ("to")
+*/
+bool seatStatus[10];                        // Taken/free. Set this to however many seats there are.
+                                            // If there are seat gaps (between classes), set this to the absolute value of secondClassSeats[1].
+                                            // A non-standart seat configuration, causes empty indexes, other problems
 string seatStatusString;                    // Taken/free
+string nameOfClass1="First class";          // Name of the first class (default: First class)
+string nameOfClass2="Economy class";        // Name of the second class (default: Economy class)
+
+/*
+Currently, seat settings do not support a non-standart seat configuration.
+Standart seat configuration can be described with these rules:
+STRICT:
+*Seat numbers of first class must be smaller than of the second class (Ignoring this will cause seats to become invalid)
+*Seat ranges must not overlap (You will not be able to reserve overlapping seats, because you will just be redirected to the other class)
+*Seat index "from" must be smaller than "to". Ignoring this will cause seats to become invalid and menu will not show any seats.
+*Sear indexes mustn't be negative
+
+NON-STRICT, but you should follow anyway:
+*Seat range should not include 0, as picking this seat will exit the program
+*There shouldn't be a gap between first class and second class as you will be able to reserve "gap" seats, however they will not be dissplayed in the reservation menu. (there is an index in the array for it, but the seat "isn't there")
+*/
 
 void printStatusOfSeats(int flightClass)    // Function to print the available seats in the selected class.
 {
     if (flightClass==1)
     {
-        cout << endl <<  "Availiable seats in First Class: " << endl;
-        for (int i=firstClassSeats[0]; i<=firstClassSeats[1]; i++)
+        cout << endl <<  "Availiable seats in the " << nameOfClass1 << ": " << endl;
+        for (int i=firstClassSeats[0]-1; i<firstClassSeats[1]; i++)
         {
             if (seatStatus[i]==0)
             {
@@ -37,14 +55,14 @@ void printStatusOfSeats(int flightClass)    // Function to print the available s
             {
                 seatStatusString = "taken";
             }
-            cout << "Seat " << i << " is " << seatStatusString << endl;
+            cout << "Seat " << i+1 << " is " << seatStatusString << endl;
         }
 
     }
     else if (flightClass==2)
     {
-        cout << endl << "Availiable seats in Economy Class: " << endl;
-        for (int i=economyClassSeats[0]; i<=economyClassSeats[1]; i++)
+        cout << endl << "Availiable seats in the " << nameOfClass2 << ": " << endl;
+        for (int i=secondClassSeats[0]-1; i<secondClassSeats[1]; i++)
         {
             if (seatStatus[i]==0)
             {
@@ -54,7 +72,7 @@ void printStatusOfSeats(int flightClass)    // Function to print the available s
             {
                 seatStatusString = "taken";
             }
-            cout << "Seat " << i << " is " << seatStatusString << endl;
+            cout << "Seat " << i+1 << " is " << seatStatusString << endl;
         }
     }
 }
@@ -63,11 +81,122 @@ void printTickets()                         // Print tickets to file
 {
 }
 
+void seatReservationMenu(int flightClass)
+{
+    if (flightClass==1) //First
+    {
+        cout << "You picked the " << nameOfClass1 << endl << endl;
+        cout << endl << "Pick a seat or enter 0 to exit:" << endl;
+        printStatusOfSeats(flightClass);
+        cin >> seatPick;
+        while (true)
+        {
+            if (seatPick==0) // Exits loop
+            {
+                break;
+            }
+            else if (seatPick<firstClassSeats[0] || seatPick>secondClassSeats[1]) // Seat not in range on flight class seats
+            {
+                cout << "Invalid seat, pick a valid one or enter 0 to exit" << endl;
+                printStatusOfSeats(flightClass);
+                cin >> seatPick;
+            }
+            else if (seatPick>=secondClassSeats[0] && seatPick<=secondClassSeats[1]) // Seat is in the other class
+            {
+                cout << "Seat is in the " << nameOfClass2 << ", would you like to pick a seat in the " << nameOfClass2 << "?" << endl << endl;
+                cout << "1 - Yes" << endl;
+                cout << "0 - No, continue picking in the " << nameOfClass1 << endl;
+                cin >> switchClass;
+                if (switchClass==1)
+                {
+                    flightClass=2;
+                    //break;
+                    seatReservationMenu(flightClass);
+
+                }
+                else if (switchClass==0)
+                {
+                    cout << "Continuing to pick in the " << nameOfClass1 << endl;
+                    printStatusOfSeats(flightClass);
+                    cin >> seatPick;
+                }
+            }
+            else if (seatStatus[seatPick-1]==1) // Seat is already taken
+            {
+                cout << endl << "Seat is already taken, pick another one or enter 0 to exit" << endl;
+                printStatusOfSeats(flightClass);
+                cin >> seatPick;
+            }
+            else // Reserves the seat
+            {
+                seatStatus[seatPick-1]=1;
+                cout << "Seat reserved" << endl;
+                cout << endl <<  "If you do not want to reserve anymore seats, input 0. Otherwise, enter the number of the seat you would like to reserve" << endl;
+                printStatusOfSeats(flightClass);
+                cin >> seatPick;
+            }
+        }
+    }
+    else if (flightClass==2) //Economy
+    {
+        cout << "You picked the " << nameOfClass2 << endl << endl;
+        printStatusOfSeats(flightClass);
+        cin >> seatPick;
+        while (true)
+        {
+            if (seatPick==0) // Exits loop
+            {
+                break;
+            }
+            else if (seatPick<firstClassSeats[0] || seatPick>secondClassSeats[1]) // Seat not in range on flight class seats
+            {
+                cout << "Invalid seat, pick a valid one or enter 0 to exit" << endl;
+                printStatusOfSeats(flightClass);
+                cin >> seatPick;
+            }
+            else if (seatPick>=firstClassSeats[0] && seatPick<=firstClassSeats[1]) // Seat is in the other class
+            {
+                cout << "Seat is in the " << nameOfClass1 << ", would you like to pick a seat in the " << nameOfClass1 << "?" << endl;
+                cout << "1 - Yes" << endl;
+                cout << "0 - No, continue picking in the " << nameOfClass2 << endl;
+                cin >> switchClass;
+                if (switchClass==1)
+                {
+                    flightClass=1;
+                    //break;
+                    seatReservationMenu(flightClass);
+
+                }
+                else if (switchClass==0)
+                {
+                    cout << "Continuing to pick in the " << nameOfClass2 << endl;
+                    printStatusOfSeats(flightClass);
+                    cin >> seatPick;
+                }
+            }
+            else if (seatStatus[seatPick-1]==1) // Seat is already taken
+            {
+                cout << endl << "Seat is already taken, pick another one or enter 0 to exit" << endl;
+                printStatusOfSeats(flightClass);
+                cin >> seatPick;
+            }
+            else // Reserves the seat
+            {
+                seatStatus[seatPick-1]=1;
+                cout << "Seat reserved" << endl;
+                cout << endl <<  "If you do not want to reserve anymore seats, input 0. Otherwise, enter the number of the seat you would like to reserve" << endl;
+                printStatusOfSeats(flightClass);
+                cin >> seatPick;
+            }
+        }
+    }
+}
+
 int main()
 {
     // Loop to set all seatStatus[] array values to 0 (seat free)
     // --------------------------- FOR LOOP START ---------------------------
-    for (int i=firstClassSeats[0]; i<economyClassSeats[1]; i++)
+    for (int i=firstClassSeats[0]; i<secondClassSeats[1]; i++)
     {
         seatStatus[i]=0;
     }
@@ -80,8 +209,8 @@ int main()
     // --------------------------- WHILE LOOP START ---------------------------
     while (true)
     {
-        cout << "\t\t\t1: First Class" << endl;
-        cout << "\t\t\t2: Economy Class" << endl;
+        cout << "\t\t\t1: " << nameOfClass1 << endl;
+        cout << "\t\t\t2: " << nameOfClass2 << endl;
         cin >> flightClass;
         if (flightClass==1 || flightClass==2)
         {
@@ -94,62 +223,6 @@ int main()
     }
     // --------------------------- WHILE LOOP END ---------------------------
 
-    if (flightClass==2)
-    {
-        //while (true)
-        //{
-            cout << endl << "Pick a seat or enter 0 to exit:" << endl;
-            printStatusOfSeats(flightClass);
-            cin >> seatPick;
-            //if (seatPick>=economyClassSeats[0] && seatPick<=economyClassSeats[1])
-            //{
-                while (true)
-                {
-                    if (seatPick==0) // Exits loop
-                    {
-                        break;
-                    }
-                    else if (seatPick<firstClassSeats[0] || seatPick>economyClassSeats[1]) // Seat not in range on flight class seats
-                    {
-                        cout << "Invalid seat, pick a valid one or enter 0 to exit" << endl;
-                        printStatusOfSeats(flightClass);
-                        cin >> seatPick;
-                    }
-                    else if (seatPick>=1 && seatPick<=5) // Seat is in the other class
-                    {
-                        cout << "Seat is in First class, would you like to pick a seat in the first class?" << endl;
-                        cout << "1 - Yes" << endl;
-                        cout << "0 - No, continue picking in Economy class" << endl;
-                        cin >> switchClass;
-                        if (switchClass==1)
-                        {
-                        }
-                        else if (switchClass==0)
-                        {
-                        }
-                    }
-                    else if (seatStatus[seatPick]==1) // Seat is already taken
-                    {
-                        cout << endl << "Seat is already taken, pick another one or enter 0 to exit" << endl;
-                        printStatusOfSeats(flightClass);
-                        cin >> seatPick;
-                    }
-                    else // Reserves the seat
-                    {
-                        seatStatus[seatPick]=1;
-                        cout << "Seat reserved" << endl;
-                        cout << endl <<  "If you do not want to reserve anymore seats, input 0. Otherwise, enter the number of the seat you would like to reserve" << endl;
-                        printStatusOfSeats(flightClass);
-                        cin >> seatPick;
-                    }
-                }
-            //}
-            /*else
-            {
-                cout << "Invalid seat, please pick between 5th and 10th seat" << endl;
-            }*/
-            //break;
-        //}
-    }
+    seatReservationMenu(flightClass);
     return 0;
 }
